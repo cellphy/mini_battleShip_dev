c4b8fdbbf440e35d833d5dd8ba5d835541b0b2c7
Darya DeBello <unkiepaul@aol.com>
2023-12-04 22:38:42 -0500

    assignment completed

--- part1.js
+++ part1.js
@@ -1,79 +1,101 @@
-const rs = require('readline-sync');
+const rs = require("readline-sync");
 
 function getRandomNum(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-function generateBoard() {
-    const board = [];
-    for (let i = 0; i < 3; i++) {
-      board.push([]); 
-      for (let j = 0; j < 3; j++) {
-        board[i].push(' ');    
-      }      
+const gridSize = 3;
+
+function generateBoard(gridSize) {
+  const board = [];
+  for (let i = 0; i < gridSize; i++) {
+    board.push([]);
+    for (let j = 0; j < gridSize; j++) {
+      board[i].push(" ");
     }
-    const ship1X = getRandomNum(3);
-    const ship1Y = getRandomNum(3);
-    board[ship1X][ship1Y] = 'S'; 
-    let ship2X = getRandomNum(3);
-    let ship2Y = getRandomNum(3);
-    while (board[ship2X][ship2Y] === 'S') {
-      ship2X = getRandomNum(3);
-      ship2Y = getRandomNum(3);
-    }
-    board[ship2X][ship2Y] = 'S';
-    return board;
+  }
+  return board;
+}
+
+function placeShips(board) {
+  const ship1X = getRandomNum(gridSize);
+  const ship1Y = getRandomNum(gridSize);
+  board[ship1X][ship1Y] = "S";
+  let ship2X = getRandomNum(gridSize);
+  let ship2Y = getRandomNum(gridSize);
+  while (board[ship2X][ship2Y] === "S") {
+    ship2X = getRandomNum(gridSize);
+    ship2Y = getRandomNum(gridSize);
+  }
+  board[ship2X][ship2Y] = "S";
 }
 
 function printBoard(board) {
-    console.log('   A  B  C');
-    console.log('  ---------');  
-    for (let i = 0; i < board.length; i++) {
-      console.log(`${i+1} | ${board[i].join(' ')} |`);
-  
-    }  
+  console.log("   1  2  3");
+  console.log("  ---------");
+  for (let i = 0; i < board.length; i++) {
+    console.log(`${String.fromCharCode(65 + i)} | ${board[i].join(" ")} |`);
+  }
+  console.log("  ---------");
 }
 
 function getGuess() {
-    const guess = rs.question('Enter a location to strike (e.g. A2): ');
-    if (!guess.match(/^[A-C][1-3]$/)) {
-      console.log('Invalid input. Please enter a letter between A and C followed by a number between 1 and 3 (e.g. A2)');
-      return getGuess();
-    } else {
-      return guess;
-    }
+  const guess = rs.question("Enter a location to strike (e.g. A2): ");
+  if (!guess.match(/^[A-C][1-3]$/)) {
+    console.log(
+      "Invalid input. Please enter a letter between A and C followed by a number between 1 and 3 (e.g. A2)"
+    );
+    return getGuess();
+  } else {
+    return guess;
+  }
 }
 
-function playGame() {
-    let board = generateBoard();
-    let numShipsRemaining = 2;  
-    console.log('Press any key to start the game.');
-    rs.keyInPause();
-  
-    while (numShipsRemaining > 0) {
-      printBoard(board);
-      const guess = getGuess();
-      const row = guess.charCodeAt(0) - 'A'.charCodeAt(0);
-      const col = parseInt(guess.charAt(1)) - 1;
-      if (board[row][col] === 'S') {
-        console.log('Hit! You have sunk a battleship. ' + (--numShipsRemaining) + ' ship remaining.');
-        board[row][col] = 'X';
-      } else if (board[row][col] === 'X') {
-        console.log('You have already picked this location. Miss!');
-      } else {
-        console.log('You have missed!');
-        board[row][col] = 'O';
-      }
-    }
+function restartGame(playGame, gridSize) {
+  const playAgain = rs.question(
+    "You have destroyed all battleships. Would you like to play again? Y/N "
+  );
 
-const playAgain = rs.question('You have destroyed all battleships. Would you like to play again? Y/N ');
-  
-  if (playAgain.toUpperCase() === 'Y') {
-    playGame();
+  if (playAgain.toUpperCase() === "Y") {
+    playGame(gridSize);
   } else {
-    console.log('Thanks for playing!');
+    console.log("Thanks for playing!");
     process.exit();
   }
 }
-playGame();
 
+const doSomething = (numShipsRemaining, board) => {
+  while (numShipsRemaining > 0) {
+    printBoard(board);
+    const guess = getGuess();
+    const row = guess.charCodeAt(0) - "A".charCodeAt(0);
+    const col = parseInt(guess.charAt(1)) - 1;
+    if (board[row][col] === "S") {
+      console.log(
+        "Hit! You have sunk a battleship. " +
+          --numShipsRemaining +
+          " ship remaining."
+      );
+      board[row][col] = "X";
+    } else if (board[row][col] === "X") {
+      console.log("You have already picked this location. Miss!");
+    } else if (board[row][col] === "O") {
+      console.log("You have already picked this location. Miss!");
+    } else {
+      console.log("You have missed!");
+      board[row][col] = "O";
+    }
+  }
+};
+
+function playGame(gridSize) {
+  let board = generateBoard(gridSize);
+  placeShips(board);
+  let numShipsRemaining = 2;
+  console.log("Press any key to start the game.");
+  rs.keyInPause();
+
+  doSomething(numShipsRemaining, board);
+  restartGame(playGame, gridSize);
+}
+playGame(gridSize);
--- part2.js
+++ part2.js
@@ -1,109 +1,149 @@
-const rs = require('readline-sync');
+const rs = require("readline-sync");
 
-function getRandomNum(max) {
-  return Math.floor(Math.random() * Math.floor(max));}
+let board = [];
+let ships = [
+  { size: 2, hits: 0 },
+  { size: 3, hits: 0 },
+  { size: 3, hits: 0 },
+  { size: 4, hits: 0 },
+  { size: 5, hits: 0 },
+];
+let guessedLocations = [];
 
-function generateBoard() {
-    const board = [];
-    for (let i = 0; i < 10; i++) {
-      board.push([]);
-      for (let j = 0; j < 10; j++) {
-        board[i].push(' ');
+function initializeBoard(size) {
+  for (let i = 0; i < size; i++) {
+    board[i] = new Array(size).fill(" ");
+  }
+}
+
+function canPlaceShip(x, y, size, direction) {
+  for (let i = 0; i < size; i++) {
+    if (direction === 0) {
+      if (typeof board[y][x + i] === "object") return false;
+    } else {
+      if (typeof board[y + i][x] === "object") return false;
+    }
+  }
+  return true;
+}
+
+function placeShips() {
+  for (let ship of ships) {
+    let placed = false;
+    while (!placed) {
+      let direction = Math.floor(Math.random() * 2);
+      let x, y;
+
+      if (direction === 0) {
+        x = Math.floor(Math.random() * (board.length - ship.size));
+        y = Math.floor(Math.random() * board.length);
+      } else {
+        x = Math.floor(Math.random() * board.length);
+        y = Math.floor(Math.random() * (board.length - ship.size));
+      }
+
+      if (canPlaceShip(x, y, ship.size, direction)) {
+        for (let i = 0; i < ship.size; i++) {
+          if (direction === 0) {
+            board[y][x + i] = ship;
+          } else {
+            board[y + i][x] = ship;
+          }
+        }
+        placed = true;
       }
     }
-    const ship1X = getRandomNum(10);
-    const ship1Y = getRandomNum(10);
-    board[ship1X][ship1Y] = 'S';
-    let ship2X = getRandomNum(10);
-    let ship2Y = getRandomNum(10);
-    while (board[ship2X][ship2Y] === 'S') {
-      ship2X = getRandomNum(10);
-      ship2Y = getRandomNum(10);
-    }
-    board[ship2X][ship2Y] = 'S';
-    let ship3X = getRandomNum(10);
-    let ship3Y = getRandomNum(10);
-    while (board[ship3X][ship3Y] === 'S') {
-      ship3X = getRandomNum(10);
-      ship3Y = getRandomNum(10);
-    }
-    board[ship3X][ship3Y] = 'S';
-    let ship4X = getRandomNum(10);
-    let ship4Y = getRandomNum(10);
-    while (board[ship4X][ship4Y] === 'S') {
-      ship4X = getRandomNum(10);
-      ship4Y = getRandomNum(10);
-    }
-    board[ship4X][ship4Y] = 'S';
-    return board;
+  }
 }
 
-const board = [
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ', ' ']
-];
-  
-function printBoard(board) {
-    console.log('    1 2 3 4 5 6 7 8 9 10 ');
-    console.log('  ----------------------- ');   
-    for (let i = 0; i < board.length; i++) {
-      console.log(`${String.fromCharCode(65+i)} | ${board[i].join(' ')} |`);
-    }  
+function printBoard() {
+  console.log("    1 2 3 4 5 6 7 8 9 10");
+  console.log("  ----------------------- ");
+  for (let i = 0; i < board.length; i++) {
+    let row = String.fromCharCode(65 + i) + " | ";
+    for (let j = 0; j < board.length; j++) {
+      row += typeof board[i][j] === "object" ? "S " : board[i][j] + " ";
+    }
+    console.log(row + "|");
+  }
+  console.log("  ----------------------- ");
 }
-printBoard(board);
+
+function restartGame() {
+  const playAgain = rs.question(
+    "You have destroyed all battleships. Would you like to play again? Y/N "
+  );
+
+  if (playAgain.toUpperCase() === "Y") {
+    playGame();
+  } else {
+    console.log("Thanks for playing!");
+    process.exit();
+  }
+}
 
 function getGuess() {
   while (true) {
-    const guess = rs.question('Enter a location to strike (e.g. A2): ');
-    if (!guess.match(/^[A-J][1-9]|10$/)) {   
-      console.log('Invalid input. Please enter a letter between A and J followed by a number between 1 and 10 (e.g. A2)');
+    const guess = rs.question("Enter a location to strike (e.g. A2): ");
+    if (!guess.match(/^[A-J][1-9]|10$/)) {
+      console.log(
+        "Invalid input. Please enter a letter between A and J followed by a number between 1 and 10 (e.g. A2)"
+      );
     } else {
       return guess;
     }
   }
 }
 
-function playGame() {
-    let board = generateBoard();
-    let numShipsRemaining = 4;  
-    console.log('Press any key to start the game.');
-    rs.keyInPause();  
-    
-    while (numShipsRemaining > 0) {
-      printBoard(board);
-      const guess = getGuess();
-      if (guess[0] < 'A' || guess[0] > 'J' || guess.slice(1) < 1 || guess.slice(1) > 10) {
-        console.log('Invalid input. Please enter a letter between A and J followed by a number between 1 and 10 (e.g. A2)');
-        continue;
-      }
-      const row = guess.charCodeAt(0) - 'A'.charCodeAt(0);
-      const col = parseInt(guess.slice(1)) - 1;  
-      if (board[row][col] === 'S') {
-        console.log('Hit! You have sunk a battleship. ' + (--numShipsRemaining) + ' ship(s) remaining.');
-        board[row][col] = 'X';
-      } else if (board[row][col] === 'X') {
-        console.log('You have already picked this location. Miss!');
+const doSomething = () => {
+  while (ships.length > 0) {
+    printBoard();
+    let guess = getGuess();
+    let x = guess.charCodeAt(0) - 65;
+    let y = parseInt(guess.slice(1)) - 1;
+
+    if (guessedLocations.includes(guess)) {
+      console.log("You have already picked this location. Miss!");
+    } else if (
+      guess[0] < "A" ||
+      guess[0] > "J" ||
+      guess.slice(1) < 1 ||
+      guess.slice(1) > 10
+    ) {
+      console.log(
+        "Invalid input. Please enter a letter between A and J followed by a number between 1 and 10 (e.g. A2)"
+      );
+      continue;
+    } else {
+      guessedLocations.push(guess);
+      if (typeof board[x][y] === "object") {
+        let ship = board[x][y];
+        ship.hits++;
+        board[x][y] = "X";
+        if (ship.hits === ship.size) {
+          console.log(
+            "Hit. You have sunk a battleship. " +
+              (ships.length - 1) +
+              " ship(s) remaining."
+          );
+          ships.splice(ships.indexOf(ship), 1);
+        } else {
+          console.log("Hit!");
+        }
       } else {
-        console.log('You have missed!');
-        board[row][col] = 'O';
+        console.log("You have missed!");
+        board[x][y] = "O";
       }
     }
-    
-    console.log('You have destroyed all battleships. Would you like to play again?');
-    const playAgain = rs.question('Enter Y for yes or N for no: ');
-  
-    if (playAgain.toUpperCase() === 'Y') {
-      playGame();
-    }
-}  
+  }
+};
+
+function playGame() {
+  console.log("Press any key to start the game.");
+  rs.keyInPause();
+  doSomething();
+  restartGame();
+}
+initializeBoard(10);
+placeShips();
 playGame();
-
-
